9db8fc18b818ae79083785dc608d708b364ef856
MatthewJDavis <matthewdavis111@gmail.com>
2020-03-02 14:50:00 -0500

    quote path

--- AADGuestUser/ci/scripts/Pester.ps1
+++ AADGuestUser/ci/scripts/Pester.ps1
@@ -1,6 +1,6 @@
 if (!(Get-Module -Name Pester -ListAvailable)) { Install-Module -Name Pester -Scope CurrentUser -Force }
 
-$testResults = Invoke-Pester -Path ../../Send-MDAzureADGuestInvitation.Tests.ps1 -PassThru
+$testResults = Invoke-Pester -Path '../../Send-MDAzureADGuestInvitation.Tests.ps1' -PassThru
         if ($testResults.FailedCount -gt 0) {
             $testResults | Format-List
             Write-Error -Message 'One or more Pester tests failed. Build cannot continue!'
