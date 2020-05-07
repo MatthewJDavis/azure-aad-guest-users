114d1f3bf7e92d7df2acd3065bea9c527c448a72
Matthew Davis <matthewdavis111@gmail.com>
2020-05-07 19:37:03 -0400

    remove old ci scripts

--- AADGuestUser/ci/scripts/PSAnalyzer.ps1
+++
@@ -1,3 +0,0 @@
-if (!(Get-Module -Name PSScriptAnalyzer -ListAvailable)) { Install-Module -Name PSScriptAnalyzer -Scope CurrentUser -Force }
-
-Invoke-ScriptAnalyzer -Path '../../Send-MDAzureADGuestInvitation.ps1' -Severity Warning
--- AADGuestUser/ci/scripts/Pester.ps1
+++
@@ -1,7 +0,0 @@
-if (!(Get-Module -Name Pester -ListAvailable)) { Install-Module -Name Pester -Scope CurrentUser -Force }
-
-$testResults = Invoke-Pester -Path '../../Send-MDAzureADGuestInvitation.Tests.ps1' -PassThru
-        if ($testResults.FailedCount -gt 0) {
-            $testResults | Format-List
-            Write-Error -Message 'One or more Pester tests failed.'
-        }
